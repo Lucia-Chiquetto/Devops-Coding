fede79cff974820d368914d5a9611647c80d0893
Lúcia Chiquetto <132361503+Lucia-Chiquetto@users.noreply.github.com>
2025-04-19 20:19:01 -0300

    uptade docker build

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
+import random
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo: bool
+
 
 @app.get("/helloword")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/endpoint")
+
+@app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": "deu certo"}
+    return {"teste": True, "num_aleatorio": random.random(0, 2000)}
+
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+
+@app.put("/estudantes/update/{id_estudatante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+
+@app.delete("/estudante/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
+
 
